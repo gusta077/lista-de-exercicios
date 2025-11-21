8d968f8e244387d56ef2cf09dfa825f8f796fa68
gusta077 <gustavobatistadeoliveira2008@gmail.com>
2025-11-21 19:57:16 -0300

    .

---
+++ 7d.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function InputTempoReal(){
+  const [texto, setTexto] = useState('');
+  const textom = texto.length;
+
+  return (
+    <div>
+      <input
+        type="text" maxLength={50}
+        value={texto}
+        onChange={(e) => setTexto(e.target.value)}
+        placeholder='Digite algo...'/>
+      <h2> Você digitou: {texto} Total de caracteres:{textom}/50</h2>
+    </div>
+  );
+}
+
+export default InputTempoReal;
