d76ab95e7372fcd0d733c55d49c681776bd67e2a
gusta077 <gustavobatistadeoliveira2008@gmail.com>
2025-12-02 09:57:56 -0300

    Revert "Update 1a.jsx"
    This reverts commit dbd85f9fba39a0dab4ba8b90fc4be641f59fc2da.

--- 1a.jsx
+++ 1a.jsx
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
+import react from 'react';
+function olaMundo(){
+    return <h1>Bem vindo ao react</h1>;
+}
+export default olaMundo;
+=======
 import React from 'react';
 function App() {
   return (
@@ -7,3 +14,4 @@ function App() {
   );
 }
 export default App;
+>>>>>>> 8afef12dea4e102a44035f993bd89961f2c1e508
